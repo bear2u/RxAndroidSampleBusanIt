1b06d2e46e70e3a7d10a88b965c82bd9377caa28
kim <bear2u@naver.com>
2019-10-12 14:14:47 +0900

    add observeOn

--- app/src/main/java/com/example/rxandroidsample/MainActivity.java
+++ app/src/main/java/com/example/rxandroidsample/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rxandroidsample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
@@ -10,8 +11,11 @@ import android.widget.Toast;
 import com.jakewharton.rxbinding3.view.RxView;
 
 import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 import kotlin.Unit;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,12 +26,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RxView.clicks(findViewById(R.id.btn))
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .doOnNext(new Consumer<Unit>() {
+                    @Override
+                    public void accept(Unit unit) throws Exception {
+                        Log.d("Main", Thread.currentThread().getName());
+                    }
+                })
+                .observeOn(Schedulers.io())
                 .map(new Function<Object, Object>() {
                     @Override
                     public Object apply(Object o) throws Exception {
                         return "clicked";
                     }
                 })
+                .doOnNext(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        Log.d("Main", Thread.currentThread().getName());
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Consumer<Object>() {
                     @Override
                     public void accept(Object emit) throws Exception {
